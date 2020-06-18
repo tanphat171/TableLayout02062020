b38578d4fe9f987bc54658cafaee19b4833243b4
tanphat171 <phatht171@gmail.com>
2020-06-18 18:16:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tablelayout02062020/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tablelayout02062020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
